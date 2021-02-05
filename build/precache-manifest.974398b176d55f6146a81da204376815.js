eb6e21287695145b937d8e2a8729e9b706d1b14e
Bhuwan Acharya <bhuwanacharya56@gmail.com>
2021-02-05 22:14:25 +0100

    Add files via upload

---
+++ build/precache-manifest.974398b176d55f6146a81da204376815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7828637aee3f0aeb55dc32a11bdcb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0e4130452c7ac586d9a",
+    "url": "/static/css/main.a44d4aa5.chunk.css"
+  },
+  {
+    "revision": "b5a99b81e43d98e30c35",
+    "url": "/static/js/2.ee72bb2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ee72bb2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e4130452c7ac586d9a",
+    "url": "/static/js/main.2a197740.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
